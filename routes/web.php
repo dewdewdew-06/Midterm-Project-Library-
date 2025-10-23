09f7f7ce2ee2ba0cecebf425de9214f2de15be05
UnSmartest <kwidhi@gmail.com>
2025-10-23 19:47:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,24 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\AuthController;
 
-//  Home Page
+//homepage
 Route::get('/', function () {
     return view('home');
 });
 
-//  Authentication Routes
+//route for authentication
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-//  Protected Routes (Only accessible when logged in)
+//must become user to access these
 Route::middleware('auth')->group(function () {
     
-    //  Books Routes
+    //book
     Route::resource('books', BookController::class);
 
-    // Author Routes
+    //author
     Route::resource('authors', AuthorController::class);
 });
